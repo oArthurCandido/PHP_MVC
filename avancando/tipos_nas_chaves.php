2a5195684174f7245ea496b4677d66b0edf5e56c
Arthur Candido <oarthurcandido@gmail.com>
2023-05-08 16:22:46 -0300

    :running: tipos nas chaves de arrays

---
+++ avancando/tipos_nas_chaves.php
@@ -0,0 +1,14 @@
+<?php
+
+$array = [
+  1=> 'a',
+  '1' =>'b',
+  2 => 'e'
+];
+
+foreach($array as $item){
+  echo $item. PHP_EOL;
+
+}
+
+//https://www.php.net/manual/pt_BR/language.types.array.php
