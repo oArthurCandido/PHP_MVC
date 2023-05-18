50461755256b1886bb54071102d8d6a12ad23a3c
Arthur Candido <oarthurcandido@gmail.com>
2023-05-18 13:35:01 -0300

    :runner: verificando a existencia de um elemento

--- manipulando_colecoes_com_arrays/verificando_existencia.php
+++ manipulando_colecoes_com_arrays/verificando_existencia.php
@@ -4,3 +4,8 @@ $notasAssociadas = ['Arthur'=> 10,'Pedro'=>7, 'Ana'=> 9, 'Vinicius'=>null];
 
 var_dump (array_key_exists('Vinicius', $notasAssociadas));
 var_dump(isset( $notasAssociadas['Arthur']));
+var_dump(in_array(2, $notasAssociadas));
+
+$nota = 10;
+echo("Quem tirou $nota?").PHP_EOL;
+echo array_search($nota, $notasAssociadas);
