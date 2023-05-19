9cbff7332822f1e3f4a7737fe7b4b7c7093b2cde
Arthur Candido <oarthurcandido@gmail.com>
2023-05-19 09:26:45 -0300

    :tada: novo curso PHP

---
+++ strings_manipulando_textos_com_php.php/novidades/string_dentro_da_outra.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$nome = 'Arthur Candido';
+
+$ehDaMInhaFamilia = str_contains($nome, 'Candido');
+
+if($ehDaMInhaFamilia){
+  echo "$nome é da minha família".PHP_EOL;
+}else{
+  echo"Não é da minha família".PHP_EOL;
+}
+
+
+
