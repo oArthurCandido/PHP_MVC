b02f5cfa5e80f99cb2278799d4b3a96b6f87bc87
Arthur Candido <oarthurcandido@gmail.com>
2023-05-19 11:08:41 -0300

    :runner: 01_separando_nome_e_sobrenome

---
+++ strings_manipulando_textos_com_php.php/03_dividindo_e_unindo/01_separando_nome_e_sobrenome.php
@@ -0,0 +1,20 @@
+<?php
+$nome = 'Arthur Candido';
+$email =  "Árthur@alura.com.br";
+$senha = 'áéíóú';
+
+echo "Tamanho da senha: ",strlen($senha).PHP_EOL;
+echo "Tamanho da senha com MultiByte: ",mb_strlen($senha).PHP_EOL;
+
+$posicaoDoArroba = strpos($email, '@');
+
+$usuario = substr($email,0, $posicaoDoArroba);
+
+
+echo strtoupper($usuario).PHP_EOL;
+echo strtolower($usuario).PHP_EOL;
+echo substr($email,$posicaoDoArroba);
+
+var_dump(explode(' ', $nome));
+
+
